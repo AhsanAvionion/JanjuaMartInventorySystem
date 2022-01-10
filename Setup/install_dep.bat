cfb1b46a9740feb646bc5cb46060335f6a4626b1
Ahsan Mehmood <Ahsan.mehmood@outlook.com>
2022-01-11 01:58:33 +0500

    Update install_dep.bat

--- Setup/install_dep.bat
+++ Setup/install_dep.bat
@@ -16,17 +16,25 @@ echo "Install for printing services"
 pip install pywin32-303-cp39-cp39-win_amd64.whl
 pip install win32printing-0.1.3.tar.gz
 
-
+echo "Install Receipt template"
 pip install six-1.16.0-py2.py3-none-any.whl
 pip install lxml-4.7.1-cp39-cp39-win_amd64.whl
 pip install python-docx-0.8.11.tar.gz
 pip install docxcompose-1.3.4.tar.gz
-
-pip install Jinja2-3.0.3-py3-none-any.whl
 pip install MarkupSafe-2.0.1-cp39-cp39-win_amd64.whl
-
+pip install Jinja2-3.0.3-py3-none-any.whl
 pip install docxtpl-0.15.1-py2.py3-none-any.whl
- 
+
+pip install kiwisolver-1.3.2-cp39-cp39-win_amd64.whl
+pip install python_dateutil-2.8.2-py2.py3-none-any.whl
+pip install numpy-1.22.0-cp39-cp39-win_amd64.whl
+pip install fonttools-4.28.5.zip
+pip install Pillow-9.0.0-cp39-cp39-win_amd64.whl
+pip install pyparsing-3.0.6-py3-none-any.whl
+pip install packaging-21.3-py3-none-any.whl
+pip install matplotlib-3.5.1-cp39-cp39-win_amd64.whl
+
+
 
 pause
 

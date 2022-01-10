e493f52d95fc2e83f29fb5264c8b5164f3c729eb
Ahsan Mehmood <Ahsan.mehmood@outlook.com>
2022-01-11 01:45:50 +0500

    Offline Installation

---
+++ Setup/install_dep.bat
@@ -0,0 +1,32 @@
+
+echo "updating pip version"
+pip install pip-21.3.1-py3-none-any.whl
+
+echo "Install PySide6 for qt designer"
+pip install PySide6-6.2.2.1-6.2.2-cp36.cp37.cp38.cp39.cp310-none-win_amd64.whl
+pip install shiboken6-6.2.2.1-6.2.2-cp36.cp37.cp38.cp39.cp310-none-win_amd64.whl
+
+echo "Install PyQt6 for python qt GUI"
+pip install PyQt6-6.2.2-cp36-abi3-win_amd64.whl
+pip install PyQt6_sip-13.2.0-cp39-cp39-win_amd64.whl
+pip install PyQt6_Qt6-6.2.2-py3-none-win_amd64.whl
+
+
+echo "Install for printing services"
+pip install pywin32-303-cp39-cp39-win_amd64.whl
+pip install win32printing-0.1.3.tar.gz
+
+
+pip install six-1.16.0-py2.py3-none-any.whl
+pip install lxml-4.7.1-cp39-cp39-win_amd64.whl
+pip install python-docx-0.8.11.tar.gz
+pip install docxcompose-1.3.4.tar.gz
+
+pip install Jinja2-3.0.3-py3-none-any.whl
+pip install MarkupSafe-2.0.1-cp39-cp39-win_amd64.whl
+
+pip install docxtpl-0.15.1-py2.py3-none-any.whl
+ 
+
+pause
+
